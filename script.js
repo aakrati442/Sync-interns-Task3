9f99daa6f2450f3c9dc814058dc09d79b2dd2683
AAKRATI GUPTA <107616649+aakrati442@users.noreply.github.com>
2023-05-21 00:05:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,28 +22,22 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
+        img : 'https://img.freepik.com/free-vector/musical-notes-frame-with-text-space_1017-32857.jpg',
         name : 'Stay',
         artist : 'The Kid LAROI, Justin Bieber',
         music : 'music/stay.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
+        img : 'https://img.freepik.com/free-vector/musical-notes-frame-with-text-space_1017-32857.jpg',
         name : 'Falling Down',
         artist : 'Wid Cards',
         music : 'music/fallingdown.mp3'
     },
     {
-        img : 'images/faded.png',
+        img : 'https://img.freepik.com/free-vector/musical-notes-frame-with-text-space_1017-32857.jpg',
         name : 'Faded',
         artist : 'Alan Walker',
         music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
     }
 ];
 
